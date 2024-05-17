5ce5f41a4f83f92e8775250e210458fa839e6ebe
kibarke <kibarkework@gmail.com>
2024-05-17 10:04:34 -0400

    defined columns

--- models/category.js
+++ models/category.js
@@ -7,7 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    category_id: {
+      type: DataTypes.INTEGER, // Data types are set to integers
+      allowNull: false, // Every value must be filled
+      primaryKey: true, // Every value must be unique
+      autoIncrement: true,
+    }
   },
+    category_name: {
+      type: DataTypes.STRING, // Data types are set to strings
+    },
   {
     sequelize,
     timestamps: false,
