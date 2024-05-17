648c5f4831046242b2f4ddbf8d72f2bd936d5c29
kibarke <kibarkework@gmail.com>
2024-05-17 12:10:23 -0400

    building the routes but I don't know what to put for the create stuff

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { where } = require('sequelize');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -6,15 +7,30 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData)
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOneId({
+    where: {
+      id: req.params.id
+    },
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.bulkCreate([
+    {
+      // tbd
+    }
+  ])
 });
 
 router.put('/:id', (req, res) => {
